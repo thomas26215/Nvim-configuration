cd098878daef0b0a98e5ff4655a493766d6cc700
Thomas <venouilthomas123456@gmail.com>
2025-01-23 10:45:37 +0100

    Installation des plugins tree-sitter et nvim-tree

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
-vim.g.mapleader= " "
+vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -18,6 +18,35 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {}
-local plugins = {}
+local plugins = {
+    {
+        'nvim-telescope/telescope.nvim', 
+        tag = '0.1.8',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter", 
+        build = ":TSUpdate"
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end,
+    },
+}
 
 require("lazy").setup(plugins, opts)
+
+-- Ajout du raccourci pour NvimTree
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+-- Configuration des icônes personnalisées
+local web_devicons = require("nvim-web-devicons")
+
+
